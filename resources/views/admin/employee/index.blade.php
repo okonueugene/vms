0c7942d6cab7cd21ba4c74c10ed182dbd799a256
okonueugene <tylarrex67@gmail.com>
2023-11-17 09:10:29 +0300

    resize table

--- resources/views/admin/employee/index.blade.php
+++ resources/views/admin/employee/index.blade.php
@@ -18,7 +18,7 @@
                                         class="fas fa-plus"></i> {{ __('employee.add_employee') }}</a>
                                 <div
                                     style="position: relative;
-                                    right: -65%;overflow:hidden;">
+                                    right: -63%;overflow:hidden;">
                                     <a href="javascript:void(0)" id="importCasual"
                                         class="btn btn-icon icon-left btn-success float-end" data-toggle="modal"
                                         data-target="#exportCasualModal"><i class="fas fa-plus float-end"></i>Import
